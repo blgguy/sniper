d3e63597379a5cbc702d17389209ed74b0403438
A. Bulangu <bulangu97@gmail.com>
2023-05-07 22:04:05 +0100

    added some methods (INSERT / DELETE / RANDOM GENERATOR)

--- OOP/select.class.php
+++ OOP/select.class.php
@@ -6,3 +6,99 @@
  * @Fetch all
  * @Search queries
  */
+
+ class Engine extends DBCLASS
+ {
+
+    public function delete($table, $ky)
+    {
+        $id = "";
+        foreach ($ky as $key => $value) {
+            $id .= "`".$key ."` = '".$value."'";    
+        }
+
+        $sql = "UPDATE `".$table."` SET `status` = '200' WHERE ".$id;
+        $query  =   $this->Jigo->query($sql);
+
+        if($query->num_rows > 0){
+            if ($query) {
+                return true;
+            }
+        }else{
+            return false;
+        }
+    }
+
+    public function coutTables($table)
+    {
+        $sql = "SELECT * FROM `$table`";
+        $query = $this->Jigo->query($sql);
+        $tableCount = $query->num_rows;
+
+        return $tableCount;
+    }
+
+    public function Insert($table, $fields){
+        $sql    =   "";
+        $sql    .=  "INSERT INTO `".$table;
+        $sql    .=  "` (`".implode("`,`", array_keys($fields))."`) VALUES ";
+        $sql    .=  "('".implode("','", array_values($fields))."')";
+        $query  =   $this->Jigo->query($sql);
+
+        if ($query) {
+            return true;
+        }else{
+            false;
+        }
+
+    }   
+
+    function genRandomKeys(INT $limit = 6)
+    {
+        $Dtt    = time();
+        $keys   = '987654321ABCDEFGHKMNPQRSTUWYZ'.$Dtt.rand(2346,9178);
+        return  substr(str_shuffle($keys), 0, $limit);
+    }
+
+    public function update($table, $fields, $ky)
+    {
+        //$id = "";
+        $codition = "";
+        foreach ($fields as $key => $value) {
+            $codition .= "`".$key. "` = '".$value."', ";
+        }
+
+        $codition = substr($codition, 0, -2);
+
+        $sql = "UPDATE `".$table."` SET ".$codition." WHERE `id` =".$ky;
+        $query  =   $this->Jigo->query($sql);
+        if ($query) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function viewById($table, $ky)
+    {
+        $id = "";
+        foreach ($ky as $key => $value) {
+            // Update table SET name='' , qlt ='' where id = '' 
+            $id .= "`".$key ."` = '".$value."'";    
+        }
+        
+        $sql = "SELECT * FROM `".$table;
+        $sql .= "` WHERE ".$id;
+        $array = array();
+        $query = $this->Jigo->query($sql);
+         if($query->num_rows > 0){
+            while ($row = $query->fetch_array()) {
+                $array[] = $row;
+            }
+            return $array;
+        }else{
+            return false;
+        }
+    }
+ }
+ 
